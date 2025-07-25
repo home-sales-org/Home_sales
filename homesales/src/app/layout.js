1b459cf13e9983ca65f96638215705eea460a611
kasra <kasramajdy81@gmail.com>
2025-07-25 12:03:58 +0330

    Build: Start Project

---
+++ homesales/src/app/layout.js
@@ -0,0 +1,28 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata = {
+  title: "Home Sales | Real Estate Website",
+  description: "A modern real estate frontend for browsing, filtering, and exploring home listings.",
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
